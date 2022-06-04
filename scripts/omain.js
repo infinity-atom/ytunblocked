d11ff75aaa3a1b02b409c277f58abf97de62277b
infinity-atom <50982527+infinity-atom@users.noreply.github.com>
2022-06-04 13:46:26 +1000

    Add files via upload

--- scripts/omain.js
+++ scripts/omain.js
@@ -8,25 +8,4 @@ function showMenu() {
     }
 }
 
-function getParam(name, url = window.location.href) {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-
-var userResponse = "none";
-const bsfOf = "cHZwbGVnYWN5";
-
-function askPass() {
-    userResponse = prompt("Enter Password");
-    if (userResponse === atob(bsfOf)) {
-        return true;
-    } else {
-        askPass();
-    }
-}
-
-askPass();
+//base64 funcs: btoa, atob
